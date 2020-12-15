e5116a86c571e2081404416e4ed8a3bdc6087c67
CJM <571574085@qq.com>
2020-12-15 17:43:30 +0800

    完成"我的" 页面

--- client/pages/mine/mine.js
+++ client/pages/mine/mine.js
@@ -2,11 +2,27 @@ var app = getApp();
 
 Page({
   data: {
-    memberId:'',
-    userId:'',
-    nickName:'',
-    avatar:'',
-    memberFlag:''
+    listData: [
+      {
+        laizi: "小明",
+        time: "12月11日18：40",
+        fanli: "85.00",
+        xiaofei: "98.00",
+      },
+      {
+        laizi: "小红",
+        time: "12月12日18：40",
+        fanli: "45.00",
+        xiaofei: "58.00",
+      }
+    ],
+    hasContent: true,
+    showPayBtn: false,
+    memberId: '',
+    userId: '',
+    nickName: '',
+    avatar: '',
+    memberFlag: ''
   },
 
   onLoad() {
@@ -16,11 +32,11 @@ Page({
 
     var that = this;
 
-    my.getAuthCode({
-    scopes: 'auth_base', // 主动授权（弹框）：auth_user，静默授权（不弹框）：auth_base
-    success: (res) => {
+    false && my.getAuthCode({
+      scopes: 'auth_base', // 主动授权（弹框）：auth_user，静默授权（不弹框）：auth_base
+      success: (res) => {
 
-    if (res.authCode) {
+        if (res.authCode) {
           // console.log(app.serverUrl + '/login/' + res.authCode);
           // 调用自己的服务端接口，让服务端进行后端的授权认证
 
@@ -31,42 +47,42 @@ Page({
 
           my.request({
             url: url,
-            method: 'POST', 
-            data: { 
-              authCode: res.authCode 
+            method: 'POST',
+            data: {
+              authCode: res.authCode
             },
             headers: {
               'Content-Type': 'application/x-www-form-urlencoded'
             },
-            success: function(resdata) {
+            success: function (resdata) {
 
-              if (resdata.data.data.userId){
+              if (resdata.data.data.userId) {
 
                 app.globalData.userId = resdata.data.data.userId;
 
               }
 
-              if (resdata.data.code == 0){
+              if (resdata.data.code == 0) {
 
                 that.setData({
-                  memberId:resdata.data.data.id,
+                  memberId: resdata.data.data.id,
                   userId: resdata.data.data.userId,
-                  nickName:resdata.data.data.nickName,
-                  avatar:resdata.data.data.avatar,
-                  memberFlag:resdata.data.data.memberFlag
+                  nickName: resdata.data.data.nickName,
+                  avatar: resdata.data.data.avatar,
+                  memberFlag: resdata.data.data.memberFlag
                 });
 
-                if (resdata.data.data.memberFlag == 0){
+                if (resdata.data.data.memberFlag == 0) {
 
                 }
 
-              }else if (resdata.data.code == 1){
+              } else if (resdata.data.code == 1) {
 
                 my.navigateTo({
                   url: '/pages/GetPhoneNumber/GetPhoneNumber'
                 });
 
-              }else if (resdata.data.code == 2){
+              } else if (resdata.data.code == 2) {
 
                 my.navigateTo({
                   url: '/pages/GetUserBase/GetUserBase'
@@ -74,13 +90,13 @@ Page({
 
               }
 
-            }, 
-            fail: function(resdata) {
-               console.log(resdata);
+            },
+            fail: function (resdata) {
+              console.log(resdata);
             }
           });
 
-         
+
         }
 
       },
@@ -88,40 +104,48 @@ Page({
 
   },
 
+  tapShowPay() {
+    this.setData({ showPayBtn: true })
+  },
 
-  buyCard(){
+  tapHidePay() {
+    this.setData({ showPayBtn: false })
+  },
+
+
+  buyCard() {
 
     var url = app.serverUrl + '/aliMember/buyCard';
 
     my.request({
       url: url,
-      method: 'POST', 
-      data: { 
-        userId: app.globalData.userId 
+      method: 'POST',
+      data: {
+        userId: app.globalData.userId
       },
       headers: {
         'Content-Type': 'application/x-www-form-urlencoded'
       },
-      success: function(resdata) {
+      success: function (resdata) {
 
 
-        if (resdata.data.code == 0){
+        if (resdata.data.code == 0) {
 
           var tradeNo = resdata.data.data.tradeNo;
 
           my.tradePay({
-            tradeNO: tradeNo,  
-            success: function(res) {            
-                
+            tradeNO: tradeNo,
+            success: function (res) {
+
             },
-            fail: function(res) {  
-                
+            fail: function (res) {
+
             },
           });
 
-        }else{
+        } else {
 
-         my.showToast({
+          my.showToast({
             type: 'fail',
             content: '会通会员失败，稍后重试',
             duration: 1000,
@@ -130,37 +154,37 @@ Page({
           });
         }
 
-      }, 
-      fail: function(resdata) {
-          console.log(resdata);
+      },
+      fail: function (resdata) {
+        console.log(resdata);
       }
     });
-      
+
   },
 
   alipayScan() {
     // 打开支付宝扫一扫
     my.ap.navigateToAlipayPage({
-        appCode:'alipayScan',
-        success:(res) => {
-            // my.alert({content:'成功：'+JSON.stringify(res)});
-        },
-        fail:(res) => {
-            // my.alert({content:'失败：'+JSON.stringify(res)});
-        }
+      appCode: 'alipayScan',
+      success: (res) => {
+        // my.alert({content:'成功：'+JSON.stringify(res)});
+      },
+      fail: (res) => {
+        // my.alert({content:'失败：'+JSON.stringify(res)});
+      }
     });
   },
 
   payCode() {
     // 打开付款码
     my.ap.navigateToAlipayPage({
-        appCode:'payCode',
-        success:(res) => {
-            // my.alert({content:'成功：'+JSON.stringify(res)});
-        },
-        fail:(res) => {
-            // my.alert({content:'失败：'+JSON.stringify(res)});
-        }
+      appCode: 'payCode',
+      success: (res) => {
+        // my.alert({content:'成功：'+JSON.stringify(res)});
+      },
+      fail: (res) => {
+        // my.alert({content:'失败：'+JSON.stringify(res)});
+      }
     });
   },
 
