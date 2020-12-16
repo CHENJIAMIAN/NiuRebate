70d117ec6c08e5efb087746ae4afe364623798ae
CJM <571574085@qq.com>
2020-12-16 09:36:09 +0800

    暂存

--- .tea/entryFiles-development/index$.web.js
+++ .tea/entryFiles-development/index$.web.js
@@ -1,6 +1,7 @@
 require('@alipay/appx-compiler/lib/sjsEnvInit');
 require('./config$');
 
+require('../../client/pages/vip-invite/vip-invite?hash=32d7d2807ed4e666ef03b4b3fe8c38ecf2e34e68');
 require('../../client/pages/index/index?hash=32d7d2807ed4e666ef03b4b3fe8c38ecf2e34e68');
 require('../../client/pages/mine/mine?hash=32d7d2807ed4e666ef03b4b3fe8c38ecf2e34e68');
 require('../../client/pages/GetPhoneNumber/GetPhoneNumber?hash=32d7d2807ed4e666ef03b4b3fe8c38ecf2e34e68');
