0dea0f2d77b2e114bb745f3f781639ed39fe46bd
CJM <571574085@qq.com>
2020-12-16 18:36:45 +0800

    完成vip邀请页面

--- client/pages/vip-invite/vip-invite.js
+++ client/pages/vip-invite/vip-invite.js
@@ -1,4 +1,51 @@
 Page({
-  data: {},
-  onLoad() {},
+  data: {
+    dpListData: [{
+      img: 'https://gw.alipayobjects.com/mdn/rms_eb2664/afts/img/A*bFuBQZuNErMAAAAAAAAAAABkARQnAQ',
+      dianming: "辣椒胡",
+      type: "火锅烧烤",
+      position: "余杭区",
+      distance: "14.6",
+      price: "54.5",
+    },
+    {
+      img: 'https://gw.alipayobjects.com/mdn/rms_eb2664/afts/img/A*bFuBQZuNErMAAAAAAAAAAABkARQnAQ',
+      dianming: "辣椒胡",
+      type: "火锅烧烤",
+      position: "余杭区",
+      distance: "14.6",
+      price: "54.5",
+    }],
+    hdListData: [
+      {
+        img: 'https://gw.alipayobjects.com/mdn/rms_eb2664/afts/img/A*bFuBQZuNErMAAAAAAAAAAABkARQnAQ',
+        name: "向西系：中秋国庆限时特惠",
+      }, {
+        img: 'https://gw.alipayobjects.com/mdn/rms_eb2664/afts/img/A*bFuBQZuNErMAAAAAAAAAAABkARQnAQ',
+        name: "向西系：中秋国庆限时特惠",
+      },
+    ],
+    gmListData: [
+      {
+        img: 'https://gw.alipayobjects.com/mdn/rms_eb2664/afts/img/A*bFuBQZuNErMAAAAAAAAAAABkARQnAQ',
+        name: '小王',
+        job: '电商运营',
+        month: '1',
+        monney: '２００',
+        desc: '真的很划算，一年才１９.９元，和朋友出去吃一顿饭就能赚回来',
+      }, {
+        img: 'https://gw.alipayobjects.com/mdn/rms_eb2664/afts/img/A*bFuBQZuNErMAAAAAAAAAAABkARQnAQ',
+        name: '小王',
+        job: '电商运营',
+        month: '1',
+        monney: '２００',
+        desc: '真的很划算，一年才１９.９元，和朋友出去吃一顿饭就能赚回来',
+      },
+    ],
+  },
+  onLoad() {
+    // my.pageScrollTo({
+    //   scrollTop: parseInt(600),
+    // });
+  },
 });
