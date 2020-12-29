9695736273bd8063d33db4e9ef0eca78d2eb7e80
CJM <571574085@qq.com>
2020-12-29 18:05:02 +0800

    完成名称验证 提现页面

---
+++ pages/validate-name/validate-name.js
@@ -0,0 +1,22 @@
+Page({
+  data: {
+    inputValue: '',
+  },
+  onLoad() { },
+  titleClick() {
+    my.alert({
+      title: 'onActionTap 回调',
+      content: '标题后面操作区域点击',
+    });
+  },
+
+  bindKeyInput(e) {
+    this.setData({
+      inputValue: e.detail.value,
+    });
+  },
+
+  tapValidate(){
+    // alert(this.data.inputValue)
+  }
+});
