09d1af003086287100463283c99723614b973d93
CJM <571574085@qq.com>
2021-01-01 14:58:11 +0800

    "提现记录 做成上拉分页的形式"

--- pages/withdraw/withdraw.js
+++ pages/withdraw/withdraw.js
@@ -2,8 +2,9 @@ var app = getApp();
 Page({
   data: {
     walletMoney: '',
-    repayInput:'',
-    list:[]
+    repayInput: '',
+    list: [],
+    page: 1
   },
   onLoad() { },
   onReady() {
@@ -11,11 +12,11 @@ Page({
     var that = this;
     that.requestMemberData();
   },
-  
+
   //获取余额
   requestMemberData() {
     var url = app.serverUrl + '/cardMemberCashOut/getMemberWallet';
-    var that=this;
+    var that = this;
     my.showLoading();
     my.request({
       url: url,
@@ -26,18 +27,18 @@ Page({
       headers: {
         'Content-Type': 'application/json'
       },
-      success: (resdata) => { 
+      success: (resdata) => {
         my.hideLoading();
-        console.log("###"+JSON.stringify(resdata));
-        console.log("###"+JSON.stringify(resdata.data.code));
+        console.log("###" + JSON.stringify(resdata));
+        console.log("###" + JSON.stringify(resdata.data.code));
         if (resdata.data.code == 0) {
-          that.setData({ 
+          that.setData({
             walletMoney: resdata.data.data,
-            repayInput:'',
-            list:[]
+            repayInput: '',
+            list: []
           });
           that.getMoneyListByNet(1);
-        } 
+        }
       },
       fail: (resdata) => {
         my.hideLoading();
@@ -49,65 +50,65 @@ Page({
  * 监听金额输入
  */
   onMoneyInput(value) {
-    console.log("####"+value);
+    console.log("####" + value);
     var reg = /^(\.*)(\d+)(\.?)(\d{0,2}).*$/g;
     if (reg.test(value)) { //正则匹配通过，提取有效文本
-      value = value.replace(reg, '$2$3$4'); 
+      value = value.replace(reg, '$2$3$4');
     }
     else { //正则匹配不通过，直接清空
       value = '';
     }
-    if (value.indexOf(".") < 0 && value != "") { 
-		value = parseFloat(value);
-	  }
-    
+    if (value.indexOf(".") < 0 && value != "") {
+      value = parseFloat(value);
+    }
+
     this.setData({ repayInput: value });
-    if(value>this.data.walletMoney){
+    if (value > this.data.walletMoney) {
       my.showToast({
-          type: 'none',
-          content: '余额不足',
-          duration: 1000,
-          success: () => {
-          },
-        });
-        return;
+        type: 'none',
+        content: '余额不足',
+        duration: 1000,
+        success: () => {
+        },
+      });
+      return;
     }
 
   },
 
   //申请提现
-  tapWithDraw(){
-    let that=this;
+  tapWithDraw() {
+    let that = this;
     console.log(that.data.repayInput);
-    if(that.data.repayInput==""){
+    if (that.data.repayInput == "") {
       my.showToast({
-          type: 'none',
-          content: "提现金额为空",
-          duration: 1000,
-          success: () => {
-          },
-        });
+        type: 'none',
+        content: "提现金额为空",
+        duration: 1000,
+        success: () => {
+        },
+      });
       return;
     }
-     var url = app.serverUrl + '/cardMemberCashOut/blank';
-     my.showLoading();
+    var url = app.serverUrl + '/cardMemberCashOut/blank';
+    my.showLoading();
     my.request({
       url: url,
       method: 'POST',
       data: {
         userId: app.globalData.memberId,
-        wallet:that.data.repayInput
+        wallet: that.data.repayInput
       },
       headers: {
         'Content-Type': 'application/json'
       },
-      success: (resdata) => { 
+      success: (resdata) => {
         my.hideLoading();
-        console.log("###"+JSON.stringify(resdata));
-        console.log("###"+JSON.stringify(resdata.data.code));
+        console.log("###" + JSON.stringify(resdata));
+        console.log("###" + JSON.stringify(resdata.data.code));
         if (resdata.data.code == 0) {
           that.requestMemberData();
-        }  
+        }
         my.showToast({
           type: 'none',
           content: resdata.data.msg,
@@ -130,43 +131,63 @@ Page({
   },
 
 
-//获取提现记录列表,接口带分页
-/**
- * 返回的数据格式
- *  {{"pageNo":1,"current_page":1,"pageSize":10,
- * "per_page":10,"total":0,"totalPage":0,"last_page":0,
- * "lastVisitTime":1609396525230,"rows":[]}} 
- */
-getMoneyListByNet(pageNo){
-   var url = app.serverUrl + '/cardMemberCashOut/loadPage';
+  //获取提现记录列表,接口带分页
+  /**
+   * 返回的数据格式
+   *  {{"pageNo":1,"current_page":1,"pageSize":10,
+   * "per_page":10,"total":0,"totalPage":0,"last_page":0,
+   * "lastVisitTime":1609396525230,"rows":[]}} 
+   */
+  getMoneyListByNet(pageNo) {
+    var url = app.serverUrl + '/cardMemberCashOut/loadPage';
     my.request({
       url: url,
       method: 'POST',
       data: {
         userId: app.globalData.memberId,
-        pageNo:pageNo,
-        pageSize:10
+        pageNo: pageNo,
+        pageSize: 10
       },
       headers: {
         'Content-Type': 'application/json'
       },
-      success: (resdata) => { 
+      success: (resdata) => {
         my.hideLoading();
-        console.log("###"+JSON.stringify(resdata));
-        console.log("###"+JSON.stringify(resdata.data.code));
+        console.log("###" + JSON.stringify(resdata));
+        console.log("###" + JSON.stringify(resdata.data.code));
         if (resdata.data.code == 0) {
-          this.setData({list: resdata.data.data.rows});
-        } 
+          this.setData({ list: resdata.data.data.rows });
+        }
       },
       fail: (resdata) => {
         my.hideLoading();
+      },
+      complete: () => {
+        console.log('getMoneyListByNet compelete')
+        this.setData({ show: false });
       }
     });
 
 
-}
+  },
 
+  /**
+  * scroll-view滑到底部触发事件
+  * @method scrollMytrip 
+  */
+  async scrollMytrip() {
+    console.log('scrollMytrip')
+    try {
+      const newPage = this.data.page + 1;
+      this.setData({ page: newPage, show: true });
 
+      console.log(newPage);
 
+      this.getMoneyListByNet(newPage);
+    } catch (e) {
+      this.setData({ show: false });
+      console.log('scrollMytrip执行异常:', e);
+    }
+  },
 
 });
