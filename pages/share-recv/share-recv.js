80df0b6b0357d1415761bba724af22da6ac74a91
高流 <hi35311336@aliyun.com>
2021-01-02 20:41:42 +0800

    提交

---
+++ pages/share-recv/share-recv.js
@@ -0,0 +1,57 @@
+var app = getApp();
+
+Page({
+  data: {},
+  onLoad(query) {
+
+
+    var shareUserId = query.shareUserId;
+
+    var that = this;
+    my.getAuthCode({
+      scopes: 'auth_base', // 主动授权（弹框）：auth_user，静默授权（不弹框）：auth_base
+      success: (res) => {
+
+        if (res.authCode) {
+          // console.log(app.serverUrl + '/login/' + res.authCode);
+          // 调用自己的服务端接口，让服务端进行后端的授权认证
+
+          var url = app.serverUrl + '/aliMember/shareRecv';
+
+          console.log(res.authCode);
+
+          my.request({
+            url: url,
+            method: 'POST',
+            data: {
+              authCode: res.authCode,
+              shareUserId: shareUserId
+            },
+            headers: {
+              'Content-Type': 'application/x-www-form-urlencoded'
+            },
+            success: function (resdata) {
+
+              my.navigateTo({
+                url: '/pages/index/index' 
+              });
+
+            },
+            fail: function (resdata) {
+
+            }
+          });
+
+
+        }
+
+      },
+
+      fail() {
+        console.log('==========');
+      },
+
+    });
+
+  },
+});
